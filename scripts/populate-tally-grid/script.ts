27f569e1037ae9c967092db29f3ad58029139ee6
pete hotchkiss <pete.hotchkiss@gmail.com>
2022-04-01 17:01:15 +0100

    🚧 cleaning up some tabs

--- scripts/populate-tally-grid/script.ts
+++ scripts/populate-tally-grid/script.ts
@@ -33,22 +33,23 @@ for (i = 0; i <= MAX_POSSIBLE_ROWS; i++ ) {
 	let newRow = createFrame({
 		height: ROW_HEIGHT,
 	});
-	newRow.layoutMode = "HORIZONTAL";
-	newRow.primaryAxisAlignItems = "SPACE_BETWEEN";
-	newRow.name = `row ${i+1}`;
+
+  newRow.layoutMode = "HORIZONTAL";
+  newRow.primaryAxisAlignItems = "SPACE_BETWEEN";
+  newRow.name = `row ${i+1}`;
 
   /** This feels super hacky. It's the only way to FORCE */
   /** the child row items to behave as 'fill container' */
-	newRow.resize(width, ROW_HEIGHT);
-	newRow.layoutAlign = "STRETCH";
-	newRow.primaryAxisSizingMode = "FIXED";
+  newRow.resize(width, ROW_HEIGHT);
+  newRow.layoutAlign = "STRETCH";
+  newRow.primaryAxisSizingMode = "FIXED";
   newRow.counterAxisAlignItems = "CENTER";
 
   /** figure out how many columns we can get in */
   let MAX_COLUMNS = (ori === "portrait" && node.width <= (MAX_COLUMN_WIDTH * 10)) ? 10 : Math.round(node.width / MAX_COLUMN_WIDTH);
 
 	// newRow.layoutGrow = 1;
-	for(j = 1; j <= MAX_COLUMNS; j++) {
+  for(j = 1; j <= MAX_COLUMNS; j++) {
     let newtext = createText({
       fontName: {
       family: "Source Sans Pro",
@@ -61,7 +62,7 @@ for (i = 0; i <= MAX_POSSIBLE_ROWS; i++ ) {
     /** render the correct number in the field */
     newtext.characters = `${(i === 0) ? (j) : (i * MAX_COLUMNS) + (j)}`;
     newRow.appendChild(newtext);
-	}
+  }
 
-	node.appendChild(newRow);
+  node.appendChild(newRow);
 }
